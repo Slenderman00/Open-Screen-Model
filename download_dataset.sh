1a18f2070bbd6b93603b3839b006da2d48bd0431
Joar Heimonen <joarheimonen@live.no>
2024-05-03 00:12:16 +0200

    more data

--- download_dataset.sh
+++ download_dataset.sh
@@ -4,10 +4,23 @@ rm dataset.zip
 
 kaggle datasets download -d tapakah68/supervisely-filtered-segmentation-person-dataset
 unzip supervisely-filtered-segmentation-person-dataset.zip
-rm rf.csv
+rm df.csv
 rm supervisely-filtered-segmentation-person-dataset.zip
-
 cd supervisely_person_clean_2667_img/supervisely_person_clean_2667_img/images
+for file in *.png; do
+    ffmpeg -i "$file" -q:v 2 "${file%.png}.jpg"
+    rm "$file"
+done
+ 
+cd supervisely_person_clean_2667_img/supervisely_person_clean_2667_img/images
+cd ../../..
+
+kaggle datasets download -d tapakah68/segmentation-full-body-tiktok-dancing-dataset
+unzip segmentation-full-body-tiktok-dancing-dataset.zip
+rm df.csv
+rm segmentation-full-body-tiktok-dancing-dataset.zip
+
+cd segmentation_full_body_tik_tok_2615_img/segmentation_full_body_tik_tok_2615_img/images
 for file in *.png; do
     ffmpeg -i "$file" -q:v 2 "${file%.png}.jpg"
     rm "$file"
