dfc2439f2158cb9501a815a7dc848659a7ddc64c
Joar Heimonen <contact@joar.me>
2025-02-07 16:49:39 +0100

    feat: papermill working

--- create_multiples.py
+++ create_multiples.py
@@ -14,17 +14,6 @@ models = [
         'epochs': 100
     },
     {
-        'name': 'openscreen-256-2-6-128-0.000125',
-        'image_resolution': 256,
-        'batch_size': 2,
-        'depth': 6,
-        'init_features': 128,
-        'learning_rate': 0.000125,
-        'debug': False,
-        'debug_print_model': True,
-        'epochs': 100
-    },
-    {
         'name': 'openscreen-512-4-6-128-0.000125',
         'image_resolution': 512,
         'batch_size': 4,
@@ -57,23 +46,58 @@ models = [
         'debug_print_model': True,
         'epochs': 100
     },
+
+    # 256x256
     {
-        'name': 'openscreen-512-2-6-128-0.00025',
-        'image_resolution': 512,
+        'name': 'openscreen-256-2-6-128-0.000125',
+        'image_resolution': 256,
         'batch_size': 2,
         'depth': 6,
         'init_features': 128,
-        'learning_rate': 0.00025,
+        'learning_rate': 0.000125,
         'debug': False,
         'debug_print_model': True,
         'epochs': 100
     },
+    {
+        'name': 'openscreen-256-4-6-128-0.000125',
+        'image_resolution': 256,
+        'batch_size': 4,
+        'depth': 6,
+        'init_features': 128,
+        'learning_rate': 0.000125,
+        'debug': False,
+        'debug_print_model': True,
+        'epochs': 100
+    },
+    {
+        'name': 'openscreen-256-2-8-128-0.000125',
+        'image_resolution': 256,
+        'batch_size': 2,
+        'depth': 8,
+        'init_features': 128,
+        'learning_rate': 0.000125,
+        'debug': False,
+        'debug_print_model': True,
+        'epochs': 100
+    },
+    {
+        'name': 'openscreen-256-2-6-256-0.000125',
+        'image_resolution': 256,
+        'batch_size': 2,
+        'depth': 6,
+        'init_features': 256,
+        'learning_rate': 0.000125,
+        'debug': False,
+        'debug_print_model': True,
+        'epochs': 100
+    }
 ]
 
 for model in models:
     output_notebook = f"{model['name']}_output.ipynb"
     pm.execute_notebook(
-        'openscreen.ipynb',  # Input notebook
+        'model.ipynb',  # Input notebook
         output_notebook,  # Output notebook
         parameters=model,  # Parameters to be injected
     )
